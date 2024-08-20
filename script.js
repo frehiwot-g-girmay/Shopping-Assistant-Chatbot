be2ccee0ebe0837dbcb5150808b5369b55337462
Frehiwot Gebrekrstos Girmay <69894124+frehiwot-g-girmay@users.noreply.github.com>
2024-08-20 08:05:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const generateResponse = (incomingChatLi) => {
             messageElement
             .classList.add("error");
             messageElement
-            .textContent = "Sure, visit our website <a href="https://faustosarlie.com/"</a> www.faustosarlie.com";
+            .textContent = ("Sure, visit our website < a heref="https://faustosarlie.com/" online store </a>";)
         })
         .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 };
